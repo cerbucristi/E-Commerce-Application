bab80732d0717feabc3f18d53ded33357adce188
biancaburghianu <biancaburghianu2001@gmail.com>
2023-12-07 15:59:20 +0200

    wip

--- ECommerce.Domain/Entities/Product.cs
+++ ECommerce.Domain/Entities/Product.cs
@@ -4,13 +4,13 @@ namespace ECommerce.Domain.Entities
 {
     public class Product : AuditableEntity
     {
-        private Product(string productName, decimal price, int stockQuantity)
+        private Product(string productName, decimal price, int stockQuantity, Guid categoryId)
         {
             ProductId = Guid.NewGuid();
             ProductName = productName;
             Price = price;
             StockQuantity = stockQuantity;
-            Categories = new List<Category>();
+            CategoryId = categoryId;
         }
 
         public Guid ProductId { get; private set; }
@@ -18,15 +18,16 @@ namespace ECommerce.Domain.Entities
         public string Description { get; private set; }
         public decimal Price { get; private set; }
         public int StockQuantity { get; private set; }
-        public List<Category> Categories { get; private set; }
+        public Guid CategoryId { get; private set; }
+        public Category Category { get; private set; }
 
-        public static Result<Product> Create(string productName, decimal price, int stockQuantity)
+        public static Result<Product> Create(string productName, decimal price, int stockQuantity, Guid categoryId)
         {
             if (string.IsNullOrWhiteSpace(productName) || price <= 0 || stockQuantity < 0)
             {
                 return Result<Product>.Failure("Invalid product data.");
             }
-            return Result<Product>.Success(new Product(productName, price, stockQuantity));
+            return Result<Product>.Success(new Product(productName, price, stockQuantity, categoryId));
         }
 
         public void SetDescription(string description)
@@ -34,10 +35,12 @@ namespace ECommerce.Domain.Entities
             Description = description;
         }
 
-        public void AddToCategory(Category category)
+        public void AddToCategory(Guid categoryId)
         {
-            Categories.Add(category);
-            category.AttachProduct(this);
+            if(categoryId != Guid.Empty)
+            {
+                CategoryId = categoryId;
+            }
         }
     }
 }
