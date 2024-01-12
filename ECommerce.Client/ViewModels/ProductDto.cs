3b405b6a733ce535ac729b4d9a6cd6089c37635b
cerbucristi <cristimadalincerbu@gmail.com>
2024-01-12 15:07:34 +0200

    implemented backend part for products

---
+++ ECommerce.Client/ViewModels/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ECommerce.Client.ViewModels
+{
+    public class ProductDto
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+
+        public string ProductDescription { get; set; } = string.Empty;
+
+        public string ProductUrl { get; set; } = string.Empty;
+
+        public float ProductPrice { get; set; }
+
+    }
+}
--- ECommerce.Client/ViewModels/ProductViewModel.cs
+++ ECommerce.Client/ViewModels/ProductViewModel.cs
@@ -2,6 +2,7 @@ namespace ECommerce.Client.ViewModels;
 
 public class ProductViewModel
 {
+    public string ProductId { get; set; }
     public string Name { get; set; }
     public string Category { get; set; }
     public float Price { get; set; }
