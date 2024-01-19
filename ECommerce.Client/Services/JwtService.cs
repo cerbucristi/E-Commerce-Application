b95085566621e72219b5cc5250042ddee7104e7c
biancaburghianu <biancaburghianu2001@gmail.com>
2024-01-19 14:52:55 +0200

    added jwt parser on fe

---
+++ ECommerce.Client/Services/JwtService.cs
@@ -0,0 +1,24 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+namespace ECommerce.Client.Services
+{ 
+    public static class JwtParser
+    {
+        public static string GetRoleFromJwt(string jwt)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(jwt) as JwtSecurityToken;
+            if (jsonToken == null || !jsonToken.Header.Alg.Equals("HS256", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException("Invalid JWT");
+            }
+
+            var roleClaim = jsonToken?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role);
+
+            return roleClaim?.Value;
+        }
+    }
+
+}
