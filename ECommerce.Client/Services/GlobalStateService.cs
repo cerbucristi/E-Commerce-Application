4b7a70ee52b5164184a01c0052071d94962b6582
biancaburghianu <biancaburghianu2001@gmail.com>
2024-01-19 12:32:42 +0200

    quick fix on wishlist

--- ECommerce.Client/Services/GlobalStateService.cs
+++ ECommerce.Client/Services/GlobalStateService.cs
@@ -3,13 +3,24 @@ namespace ECommerce.Client.Services;
 public class GlobalStateService
 { 
     private int? SavedCartCount { get; set; }
-    
     public int CartCountProperty
+
     {
         get => SavedCartCount ?? 0;
         set
         {
             SavedCartCount = value;
+            NotifyStateChanged();
+        }
+    }
+
+    private int _wishlistCount;
+    public int WishlistCountProperty
+    {
+        get { return _wishlistCount; }
+        set
+        {
+            _wishlistCount = value;
             NotifyStateChanged();
         }
     }
